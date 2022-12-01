3af15ce7417d3b32d1db9e7e4dddc2aa07dc6686
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-12-01 10:43:42 -0300

    Carrousel funcionando com overflow: hidden

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,33 @@
-// CRIANDO REPOSITÓRIO
+const nextArrow = document.getElementById('nextArrow')
+const backArrow = document.getElementById('backArrow')
+const image = document.getElementById('image')
+const img1 = './images/fusca.jpeg'
+const img2 = './images/mustang.jpeg'
+const img3 = './images/porshe.jpeg'
+
+const images = [img1, img2, img3]
+let currentImage = 0
+
+function next() {
+    currentImage++
+
+    if(currentImage === images.length){
+          currentImage = 0
+    }
+
+    image.src = images[currentImage]
+
+}
+
+function back(){
+    currentImage--
+
+    if(currentImage < 0){
+        currentImage = 2
+    }
+
+    image.src = images[currentImage]
+}
+
+nextArrow.addEventListener('click', next)
+backArrow.addEventListener('click', back)
